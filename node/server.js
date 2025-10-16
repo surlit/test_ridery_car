f3e4813f6a87edd8ff5f1b4728a85a9aa6ae9400
surlit <surlit29@gmail.com>
2025-10-15 22:08:58 -0400

    [UPD]se modifico la estructura de carpeta agregando node y odoo [ADD] se agrego el endpoint que estara esperando la peticion de odoo para hacer el registro de los logs en el modulo logs de odoo

---
+++ node/server.js
@@ -0,0 +1,88 @@
+// server.js
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+const xmlrpc = require('xmlrpc')
+const odoo_url = '192.168.100.155'
+const odoo_port = '8069'
+const db = 'test_ridery_db'
+const username = 'admin'
+const password = '123'
+
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Endpoint para recibir datos de vehículos
+app.post('/api/vehicle', (req, res) => {
+    const datas = req.body;
+
+    console.log(' Datos recibidos...');
+    
+    
+    const clientCommon = xmlrpc.createClient({
+        host: odoo_url,
+        port: odoo_port,
+        path: '/xmlrpc/2/common'
+    });
+
+    clientCommon.methodCall('authenticate',[db,username,password,{}],(err,uid) =>{
+        if(err){
+            
+
+            console.log('error al auntentificar');  
+            res.status(400).json({
+                status: 'error',
+                message: 'Ha ocurrido un error al aunteticar',
+                timestamp: new Date().toISOString()   
+            });
+            
+            
+        }
+        
+        
+        datas.map((vehicle)=>{
+            console.log('vehicle');
+            console.log(vehicle);            
+            create_logs(uid,vehicle,res)
+        })
+    })
+    
+    
+    const response = {
+        status: 'success',
+        message: 'Vehículos recibido correctamente',
+        timestamp: new Date().toISOString()
+    };
+    
+    res.status(200).json(response);
+});
+function create_logs(uid,data,res){
+    const models = xmlrpc.createClient({
+        host: odoo_url,
+        port: odoo_port,
+        path: '/xmlrpc/2/object' 
+    });
+        
+     models.methodCall('execute_kw', [
+        db,          
+        uid,                
+        password,          
+        'vehicles_logs.vehicles_logs',    
+        'create',    
+        [data], 
+        {}  
+        ], (err, result) => {
+        if(err){
+            res.status
+        }
+    });
+}
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`🚀 API de los logs de vehículos corriendo en http://localhost:${PORT}`);
+
+});
